5bceb1db2ca25082363aa353fbca3c2fb60a6805
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-19 11:11:30 +0530

    logo inserted

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Login from './components/account/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>App hello</h1>
+	  <Login/>
     </div>
   );
 }
---
+++ client/src/components/account/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@mui/material'
+const Login = () => {
+	
+	return (
+		<Box>
+			<img src='client\src\assets\logo.png' />
+		</Box>
+	)
+}
+
+export default Login
