8485b7ea05cf64513bb9965f4df46bdfdf69cc0e
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-19 23:16:04 +0530

    signup controler ready

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,3 +1,14 @@
-export const signupUser = (request, responce)=>{
-	console.log(request.body);
+import User from "../model/userModel.js";
+
+
+
+export const signupUser = async(request, responce)=>{
+	console.log('request received ---> signup');
+	try {
+		const newUser = await User.create(request.body)
+		await newUser.save();
+		return responce.status(200).json({msg: 'signup sucessfull'});
+	} catch (error) {
+		return responce.status(500).json({msg: 'something went wrong while signing up...'});
+	}
 }
