b54870723621ac11d9755a65a378e4c77309af42
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-19 19:53:17 +0530

    app configured

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+// APP CONFIGURATION 
+const app = express();
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+
+
+
+// DATABASE CONNECTION
+
+
+
+
+
+// ROUTES
+
+
+
+
+// APP PORT LISTEN
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, ()=>{
+	console.log(`server is running on port: ${PORT}`);
+})
