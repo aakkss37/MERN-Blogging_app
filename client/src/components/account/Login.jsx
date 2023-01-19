1f372fd93b3e92ba6486a4d6709ee02ad753eb06
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-19 18:34:54 +0530

    saperate style for Login/signup

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,83 +1,9 @@
 import React, { useState } from 'react'
-import { TextField, Box, Button, Typography, styled } from '@mui/material';
 import image from '../../assets/logo.png'
+import { Component, LoginContainer, Image, Wrapper, LoginButton, SignupButton, Text } from './LoginStyle.js'
+import { TextField } from '@mui/material';
 
 
-const Component = styled(Box)`
-    display: flex;
-    height: 100%;
-	margin-top: 5rem;
-    justify-content: center; 
-    align-items: flex-end;    
-    padding: 15px;
-`;
-
-const LoginContainer = styled(Box)`
-	width: 500px;
-    box-shadow: 4px 0px 4px 03px rgb(0 0 0/ 0.3);
-`
-
-const Image = styled('img')({
-	width: 250,
-	margin: 'auto',
-	padding: '50px 0 0'
-});
-
-const Wrapper = styled(Box)`
-    padding: 25px 35px;
-    display: flex;
-    flex: 1;
-    overflow: auto;
-    flex-direction: column;
-    & > div, & > button, & > p {
-        margin-top: 20px;
-    }
-`;
-
-const LoginButton = styled(Button)`
-    text-transform: none;
-    background: rgb(156 211 201);
-    color: rgb(69 81 70);
-	font-weight: 700;
-    height: 48px;
-    border-radius: 2px;
-	&:hover{
-    background: rgb(181 228 220);
-  }
-`;
-
-const SignupButton = styled(Button)`
-    text-transform: none;
-    background: #fff;
-    color: rgb(156 211 201);
-    height: 48px;
-    border-radius: 2px;
-    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
-`;
-
-const Text = styled(Typography)`
-    color: #878787;
-    font-size: 12px;
-`;
-
-// const Error = styled(Typography)`
-//     font-size: 10px;
-//     color: #ff6161;
-//     line-height: 0;
-//     margin-top: 10px;
-//     font-weight: 600;
-// `
-
-// const loginInitialValues = {
-// 	username: '',
-// 	password: ''
-// };
-
-// const signupInitialValues = {
-// 	name: '',
-// 	username: '',
-// 	password: '',
-// };
 
 
 const Login = () => {
