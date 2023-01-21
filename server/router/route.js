22cd76cd61d9675d1809c7037cfd3f4d8d5d69cc
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-21 16:24:23 +0530

    login user rout set

--- server/router/route.js
+++ server/router/route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signupUser } from "../controller/controller.js";
+import { signupUser, loginUser } from "../controller/controller.js";
 
 
 
 const router = express.Router();
-router.post('/signup', signupUser)
+router.post('/signup', signupUser);
+router.post('./login', loginUser)
 
 export default router;
