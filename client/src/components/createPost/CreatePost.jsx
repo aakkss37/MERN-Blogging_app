2a555482d00877fce922aee836bcd09dc6e330be
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-22 19:21:21 +0530

    different file for css of Create post component

--- client/src/components/createPost/CreatePost.jsx
+++ client/src/components/createPost/CreatePost.jsx
@@ -1,67 +1,9 @@
 import React, {useState} from 'react';
-import img from '../../assets/img8.jpg';
-import { Box, styled, } from '@mui/system';
-import { Button, FormControl, InputBase, TextareaAutosize } from '@mui/material';
 import { AddCircle as Add } from '@mui/icons-material';
+import img from '../../assets/img8.jpg';
+import {Container, StyledFormControl, Image, Label, StyledInputBase, StyledButton, StyledTextArea, } from './CreatePostStyle'
 
-const Container = styled(Box)(({ theme }) => ({
-	margin: '50px 0px',
-	[theme.breakpoints.down('md')]: {
-		margin: 0
-	}
-}));
 
-const StyledFormControl = styled(FormControl)`
-	display: flex;
-	flex-direction: row;
-	align-item: center;
-	justify-content: space-between;
-	height: 40px;
-	margin: 20px;
-`
-
-const Image = styled('img')({
-	width: '99%',
-	height: '50vh',
-	objectFit: 'cover'
-});
-
-const Label = styled('label')({
-	display: 'flex',
-	alignItems: 'center',
-	color: 'rgb(107 107 107)',
-	fontWeight: '500',
-	cursor: 'pointer',
-	margin: '20px',
-})
-
-const StyledInputBase = styled(InputBase)`
-	border-bottom: 2px solid rgb(107 107 107);
-	font-size: 1.5rem;
-	font-weight: 700;
-`
-
-const StyledButton = styled(Button)`
-    margin: 0px 20px;
-    background: rgb(100 210 200);
-    color: rgb(68 80 69);
-	font-weight: 600;
-	&:hover{
-		background: rgb(24 24 24);
-		color: rgb(155 210 200);
-	}
-`;
-
-const StyledTextArea = styled(TextareaAutosize)`
-	width: 91%;
-	margin: 50px;
-	font-size: 18px;
-	// border: none;
-	background-color: rgb(233, 233, 233);
-	&:focus-visible {
-		outline: none;
-	}
-`
 
 
 const initialPostData = {
