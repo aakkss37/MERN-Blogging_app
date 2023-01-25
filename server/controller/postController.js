2ccc316a34990f063861f6ffa1530813f452150f
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-25 23:26:29 +0530

    responce to api call getAllPost handled

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -2,14 +2,23 @@ import Posts from "../model/postsModel.js";
 
 export const createPost = async(request, responce)=>{
 	try {
-		console.log("****request received****")
+		// console.log("****request received****")
 		// console.log("body ==>", request.body)
 		const post = await Posts.create(request.body);
-		console.log("data posted =-=-=-=->>>>", post);
+		// console.log("data posted =-=-=-=->>>>", post);
 		post.save();
 		return responce.status(200).json({msg: "New post successfully created"});
 	} catch (error) {
 		console.log(error.message)
 		return responce.status(500).json({ msg: 'Error while creating new post...' });
+	}
+}
+
+export const getAllPosts = async(request, responce)=>{
+	try {
+		const allPost = await Posts.find();
+		responce.status(200).json({posts: allPost});
+	} catch (error) {
+		responce.status(500).json({msg: error.message});
 	}
 }
