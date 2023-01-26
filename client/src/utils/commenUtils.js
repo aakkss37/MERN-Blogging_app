0a5ed2f835d29dfe5a42e7cb31871ae7a9b4fc10
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-26 20:25:46 +0530

    elipsis added

--- client/src/utils/commenUtils.js
+++ client/src/utils/commenUtils.js
@@ -1,3 +1,9 @@
 export const getAccessToken = ()=>{
 	return sessionStorage.getItem("accessToken");
 }
+
+
+
+export const addElipsis = (string, limit)=> {
+	return string.length > limit ? string.substring(0, limit) + '...' : string;
+}
