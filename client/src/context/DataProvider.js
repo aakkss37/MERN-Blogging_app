de437bdee41a27e4f6bc6c04e5226d66af589648
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-26 20:43:04 +0530

    filter post globle state (context) created

--- client/src/context/DataProvider.js
+++ client/src/context/DataProvider.js
@@ -2,12 +2,24 @@ import { createContext, useState } from "react";
 
 export const DataContext = createContext(null);
 
+const initialCategory = {
+	category: 'all',
+}
+
+
 const DataProvider = (props)=>{
+	// USER ACCCUNT
 	const [userAccount, setUserAccount]= useState({userName: '', name: ''});
 	// console.log("loged in account ----> ", userAccount)
 
+
+	// FILTER POST
+
+	const [filterCtegory, setFilterCategory] = useState(initialCategory);
+
+
 	return (
-		<DataContext.Provider value={{userAccount, setUserAccount}}>
+		<DataContext.Provider value={{ userAccount, setUserAccount, filterCtegory, setFilterCategory }}>
 			{props.children}
 		</DataContext.Provider>
 	)
