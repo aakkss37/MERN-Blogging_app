a5d662c75826cc36c61dedf079783d5cc8ba925d
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-28 21:32:48 +0530

    about and contact page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate, Outlet,  } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, Outlet, } from 'react-router-dom'
 import DataProvider from './context/DataProvider';
 
 import Login from './components/account/Login';
@@ -10,6 +10,8 @@ import CreatePost from './components/createPost/CreatePost';
 import Error404 from './components/Error404';
 import DetailedView from './components/details/DetailedView';
 import UpdatePost from './components/updatePost/UpdatePost';
+import Contact from './components/contact/Contact';
+import About from './components/about/About';
 
 
 const PrivateRoute = (props) => (
@@ -34,28 +36,35 @@ function App () {
 			<BrowserRouter>
 				<div className="App" style={{ marginTop: 64 }}>
 					<Routes>
-						
+
 						<Route path='/login' element={<Login setUserAuthanticated={setUserAuthanticated} />} />
-						<Route path='/' element={<Navigate replace to='/login' />}/>
+						<Route path='/' element={<Navigate replace to='/login' />} />
 
 
-						<Route path='/home' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated}/>}>
+						<Route path='/home' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated} />}>
 							<Route path='/home' element={<Home />} />
 						</Route>
 
-						<Route path='/create-new-post' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated}/>}>
+						<Route path='/create-new-post' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated} />}>
 							<Route path='/create-new-post' element={<CreatePost />} />
 						</Route>
-						
-						<Route path='/update-post' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated}/>}>
+
+						<Route path='/update-post' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated} />}>
 							<Route path='/update-post' element={<UpdatePost />} />
 						</Route>
 
-						<Route path='/home/details' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated}/>}>
+						<Route path='/home/details' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated} />}>
 							<Route path='/home/details' element={<DetailedView />} />
 						</Route>
 
-						<Route path="*" element={<Error404/>} />
+						<Route path='/contact' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated} />}>
+							<Route path='/contact' element={<Contact />} />
+						</Route>
+
+						<Route path='/about' element={<PrivateRoute isUserAuthanticated={isUserAuthanticated} />}>
+							<Route path='/about' element={<About />} />
+						</Route>
+						<Route path="*" element={<Error404 />} />
 
 					</Routes>
 				</div>
